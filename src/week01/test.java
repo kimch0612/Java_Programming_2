f11ebec574a154bdc1cf59b755cfb71c4136b5cd
Changhwan Kim <kimch061279@gmail.com>
2024-09-03 14:13:48 +0900

    a

--- src/week01/test.java
+++ src/week01/test.java
@@ -2,7 +2,7 @@ package week01;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hi there!");
-        System.out.println("It's my first Java program!");
+        System.out.println("Wa!");
+        System.out.println("Sans!!");
     }
 }
