2401115ce52460b52c2db9b50aeed5dcff890883
Changhwan Kim <kimch061279@gmail.com>
2024-09-03 14:25:26 +0900

    a

--- src/week01/review.java
+++ src/week01/review.java
@@ -1,6 +1,6 @@
 package week01;
 
-public class test {
+public class review {
     public static void main(String[] args) {
         System.out.println("Wa!");
         System.out.println("Sans!!");
