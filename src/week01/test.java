333999e3f8ca4ae3177c3c6e4a48416ded9bec46
Changhwan Kim <kimch061279@gmail.com>
2024-09-03 14:22:52 +0900

    a

--- src/week01/test.java
+++ src/week01/test.java
@@ -4,5 +4,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Wa!");
         System.out.println("Sans!!");
+
     }
 }
+// int double long char boolean float short byte String
+// byte short int long char float double boolean String
