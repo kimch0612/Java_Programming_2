bab9756c2f396dad07cb906083191d4c282e4f1c
Changhwan Kim <kimch061279@gmail.com>
2024-09-10 16:51:54 +0900

    a

--- src/week02/ProductMain.java
+++ src/week02/ProductMain.java
@@ -2,13 +2,16 @@ package week02;
 
 public class ProductMain {
     public static void main(String[] args) {
-        Product product = new Product("Laptop", 100, Product.TaxCalc.KOREA);
+        Product product = new Product("Laptop",740, Product.TaxCalc.KOREA);
         product.calcPriceincludeTax();
         product.displayProductInfo();
 
-        product = new Product("Cup", 100, Product.TaxCalc.FR);
+        product = new Product("Cup", 172, Product.TaxCalc.FR);
         product.calcPriceincludeTax();
         product.displayProductInfo();
         System.out.println("*".repeat(15));
+
+        Product.TaxCalc taxcalc = product.new TaxCalc();
+        System.out.println(taxcalc.calculateTax());
     }
 }
