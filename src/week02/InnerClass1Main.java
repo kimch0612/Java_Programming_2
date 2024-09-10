2392ddf15fb37fbcb84be76007594b0e26219ce5
Changhwan Kim <kimch061279@gmail.com>
2024-09-10 15:38:17 +0900

    a

--- src/week02/InnerClass1Main.java
+++ src/week02/InnerClass1Main.java
@@ -12,10 +12,12 @@ class A {
 
     class B {
         void bcd() {
+            // 1. outer class 필드 사용
             System.out.println("a");
             System.out.println("b");
             System.out.println("c");
             System.out.println("d");
+            // 2. outer class 메소드 사용
             abc();
         }
     }
@@ -23,7 +25,11 @@ class A {
 
 public class InnerClass1Main {
     public static void main(String[] args) {
+        // 3. instance inner class 객체 생성
+        // 3.1 outer class 객체 생성
         A a = new A();
+
+        // 3.1 outer 참조 변수로부터 inner 클래스 객체 생성
         A.B b = a.new B();
         b.bcd();
     }
