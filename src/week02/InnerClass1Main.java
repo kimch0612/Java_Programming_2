2a2e8efbd0b9e0a6f91d3a9ea850f3f52c16eed0
Changhwan Kim <kimch061279@gmail.com>
2024-09-10 15:44:28 +0900

    a

--- src/week02/InnerClass1Main.java
+++ src/week02/InnerClass1Main.java
@@ -1,24 +1,41 @@
 package week02;
 
 class A {
-    public int a = 3;
-    protected int b = 4;
-    int c = 5;
-    private int d = 6;
+    // public int a = 3;
+    // protected int b = 4;
+    // int c = 5;
+    // private int d = 6;
 
+    // void abc() {
+    //     System.out.println("A 클래스 메소드");
+    // }
+
+    // class B {
+    //     void bcd() {
+    //         // 1. outer class 필드 사용
+    //         System.out.println(a);
+    //         System.out.println(b);
+    //         System.out.println(c);
+    //         System.out.println(d);
+    //         // 2. outer class 메소드 사용
+    //         abc();
+    //     }
+    // }
+    int a=3, b=4;
     void abc() {
         System.out.println("A 클래스 메소드");
     }
-
     class B {
+        int a=5, b=6;
+        void abc() {
+            System.out.println("B 클래스 메소드");
+        }
         void bcd() {
-            // 1. outer class 필드 사용
             System.out.println(a);
             System.out.println(b);
-            System.out.println(c);
-            System.out.println(d);
-            // 2. outer class 메소드 사용
-            abc();
+            System.out.println(A.this.a);
+            System.out.println(A.this.b);
+            A.this.abc();
         }
     }
 }
@@ -35,7 +52,7 @@ public class InnerClass1Main {
 
         System.out.println(a.a);
         System.out.println(a.b);
-        System.out.println(a.c);
+        // System.out.println(a.c);
         // System.out.println(a.d); private이므로 오류 발생
     }
 }
