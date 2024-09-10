612d97fec27d71d009a13dcc4ce493f22333d7ff
Changhwan Kim <kimch061279@gmail.com>
2024-09-10 16:14:36 +0900

    a

---
+++ src/week02/AA.java
@@ -0,0 +1,13 @@
+package week02;
+
+public class AA {
+    class EE {}
+    void abc() {
+        class A {}
+        class B {}
+    }
+    void bcd() {
+        class B {}
+        class C {}
+    }
+}
