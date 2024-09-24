c32ac63ae47696cb32520c851dd5be5bbb8c3a8a
Changhwan Kim <kimch061279@gmail.com>
2024-09-24 14:40:34 +0900

    a

--- src/week03/test.java
+++ src/week03/test.java
@@ -1,15 +1,31 @@
 package week03;
 
+import java.io.InputStreamReader;
+
 public class test {
     public static void main(String[] args) {
+        // try {
+        //     System.out.println(3/0);
+        // }
+        // catch(ArithmeticException e) {
+        //     System.out.println("숫자는 0으로 나눌 수 없습니다.");
+        // }
+        // finally {
+        //     System.out.println("종료합니다.");
+        // }
         try {
-            System.out.println(3/0);
-        }
-        catch(ArithmeticException e) {
-            System.out.println("숫자는 0으로 나눌 수 없습니다.");
-        }
-        finally {
-            System.out.println("종료합니다.");
+            is = new InputStreamReader(System.in);
+            System.out.println(is.read());
+        } catch (IOException e) {
+            System.out.println("Hi");
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    System.out.println("Hello");
+                }
+            }
         }
     }
 }
