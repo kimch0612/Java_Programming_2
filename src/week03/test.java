9bd45816e2407cd4862eb727e745fb143bbca46f
Changhwan Kim <kimch061279@gmail.com>
2024-09-24 14:31:26 +0900

    a

--- src/week03/test.java
+++ src/week03/test.java
@@ -2,6 +2,20 @@ package week03;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("a");
+        try {
+            System.out.println(3/0);
+        }
+        catch(ArithmeticException e) {
+            System.out.println("숫자는 0으로 나눌 수 없습니다.");
+        }
+        finally {
+            System.out.println("종료합니다.");
+        }
     }
 }
+
+// 지난 주 code review
+
+/*
+
+*/
