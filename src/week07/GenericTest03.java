300adde8935e4643724ba36ee53ca95b9bcd0608
Changhwan Kim <kimch061279@gmail.com>
2024-10-15 15:48:02 +0900

    a

--- src/week07/GenericTest03.java
+++ src/week07/GenericTest03.java
@@ -4,18 +4,7 @@ class Apple{}
 class Pencil{}
 
 class Basket<T>{
-    T obj;
-
-    public T getObj() {
-        return this.obj;
-    }
-
-    public void setObj(T obj) {
-        this.obj = obj;
-    }
-
-    public void putIn(){}
-
+    public void putIn(T something){}
 }
 
 class Goods<T> {
@@ -38,13 +27,22 @@ public class GenericTest03 {
         String Word = gs.getObj();
         System.out.println(Word);
         Goods<Integer> gs1 = new Goods<Integer>();
-
+        // 제네릭인터페이스의 타입을 스트링으로
         Basket<String> b = new Basket<String>() {
             @Override
             public void putIn(String something) {
-                // TODO Auto-generated method stub
-                super.putIn();
+                System.out.println(something + " 담았다.");
             }
         };
+        b.putIn("신발을");
+
+        //제네릭인터페이스의 타입을 애플로
+        Basket<Apple> b2 = new Basket<Apple>() {
+            @Override
+            public void putIn(Apple something) {
+                System.out.println(something + " 담았다.");
+            }
+        };
+        b2.putIn(new Apple()); // week07.Apple@8bcc55f 담았다.에서 앞부분은 고유의 해시코드임
     }
 }
