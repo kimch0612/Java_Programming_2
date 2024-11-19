2290bfd7ef772ffe10fe66c1f1c7072dedac61b5
Changhwan Kim <kimch061279@gmail.com>
2024-11-19 14:33:01 +0900

    a

---
+++ src/week12/AnimalMain.java
@@ -0,0 +1,17 @@
+package week12;
+
+interface Animal {
+    void cry();
+}
+
+public class AnimalMain {
+    public static void main(String[] args) {
+        Animal cat = new Animal() {
+            @Override
+            public void cry() {
+                System.out.println("야옹");
+            }
+        }; cat.cry();
+        Animal dog = () -> {System.out.println("멍멍");}; dog.cry();
+    }
+}
