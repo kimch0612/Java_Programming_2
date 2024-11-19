f06c6e728d5abdbbfeb114e92c723f3cfd1ce588
Changhwan Kim <kimch061279@gmail.com>
2024-11-19 15:13:50 +0900

    a

--- src/week12/AnimalMain.java
+++ src/week12/AnimalMain.java
@@ -12,6 +12,6 @@ public class AnimalMain {
                 System.out.println("야옹");
             }
         }; cat.cry();
-        Animal dog = () -> {System.out.println("멍멍");}; dog.cry();
+        Animal dog = () -> System.out.println("멍멍"); dog.cry();
     }
 }
---
+++ src/week12/test.java
@@ -0,0 +1,7 @@
+package week12;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("집가고싶다");
+    }
+}
