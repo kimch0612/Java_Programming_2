13871620c6783bdeb2b14640a53905010734896f
Changhwan Kim <kimch061279@gmail.com>
2024-11-26 14:34:28 +0900

    a

--- src/week13/LambdaExample2.java
+++ src/week13/LambdaExample2.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class LambdaExample2 {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+        // 람다식 방법
         names.forEach(name -> System.out.println(name));
+        // 인스턴스 메소드 방법
+        names.forEach(System.out::println);
     }
 }
